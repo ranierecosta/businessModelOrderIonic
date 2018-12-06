32efa311c597fd3967d5ca5ef920cbc6fb0dbb6b
raniere.costa <raniere.costa@totvs.com.br>
2018-12-06 10:15:37 -0200

    Adicionando email do token de autenticação ao localStorage e adição do jwt ao projeto vscode.

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -4,9 +4,12 @@ import { CredenciaisDTO } from './../models/credenciais.dto';
 import { Injectable } from "@angular/core";
 import { LocalUser } from "../models/local_user";
 import { StorageService } from './storage.service';
+import { JwtHelper } from 'angular2-jwt';
 
 @Injectable()
 export class AuthService {
+
+    jwtHelper: JwtHelper = new JwtHelper();
 
     constructor(public http: HttpClient, public storage: StorageService) {
 
@@ -26,7 +29,8 @@ export class AuthService {
 
         let tok = authorizationValue.substring(7);
         let user: LocalUser = {
-            token: tok
+            token: tok,
+            email: this.jwtHelper.decodeToken(tok).sub
         };
 
         this.storage.setLocalUser(user);
